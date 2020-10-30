06a0330f06f2f9b6e48ba6b9c939825ebf06d9f4
Zac Edens <themancalledzac@gmail.com>
2020-10-30 11:02:39 -0700

    Added all questions for the photography quiz.

---
+++ Assets/JS/script.js
@@ -0,0 +1,64 @@
+var timeLeft = "";
+var questions = [
+    {
+        question: "The amount of light on a photograph is known as the:",
+        choices: ["Exposure", "Shutter Speed", "Contrast", "Aperture"],
+        answer: 0
+    },
+    {
+        question: "The amount of light passing through a lens is defined by the:",
+        choices: ["Shutter Speed", "Exposure", "Aperture", "ISO"],
+        answer: 2
+    },
+    {
+        question: "What is shutter speed measured in?",
+        choices: ["Stops", "Seconds", "ISO", "Miliseconds"],
+        answer: 1
+    },
+    {
+        question: "Which is a larger aperture?",
+        choices: ["F8", "F5.6", "F2.8", "F16"],
+        answer: 2
+    },
+    {
+        question: "Taking a series of exposures at, above, and below the recommended meter reading is called:",
+        choices: ["Time Lapse", "Depth of Exposure", "Zooming", "Bracketing"],
+        answer: 3
+    },
+    {
+        question: "The camera meter measures the light on the subject to make the image:",
+        choices: ["Steady", "Focused", "Middle Gray", "Color Balanced"],
+        answer: 2
+    },
+    {
+        question: "Which of these is NOT a compositional guideline in photography?",
+        choices: ["Three seconds rule", "Rule of Thirds", "Golden Ratio", "Leading Lines"],
+        answer: 0
+    },
+    {
+        question: "When was the first color photograph taken?",
+        choices: ["1821", "1905", "1861", "1921"],
+        answer: 2
+    },
+    {
+        question: "What is the side effect of using a high ISO setting?",
+        choices: ["Battery Drain", "Digital Noise", "Color Imbalances", "Blurry Edges"],
+        answer: 1
+    },
+    {
+        question: "If, while shooting a photo you want a greater depth of field in the image, would you shoot at:",
+        choices: ["F8", "F5.6", "F2.8", "F16"],
+        answer: 3    
+    },
+    {
+        question: "When shooting in manual mode, which of the following controls the exposure of your photograph?",
+        choices: ["Aperture", "Shutter Speed", "ISO", "All of the above"],
+        answer: 3
+    },
+    {
+        question: "If your camera is set at ISO 100, how many stops of light would ISO 1600 be?",
+        choices: ["Four", "Three", "Six", "Five"],
+        answer: 0
+    }
+    
+]
